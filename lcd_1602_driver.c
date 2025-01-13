7b07df1edfb5517c82040b2dfec9fb6f3a4c5d66
Simar Singh Ubhi <73083235+simarubhi@users.noreply.github.com>
2025-01-13 17:23:51 -0500

    Names and bugs fixed source file

--- lcd_1602_driver.c
+++ lcd_1602_driver.c
@@ -24,33 +24,33 @@ void init_lcd(I2C_HandleTypeDef *hi2c)
 
     // Start sequence
 	HAL_Delay(25);
-	send_command(0x30);
+	send_lcd_command(0x30);
 	HAL_Delay(6);
-	send_command(0x30);
+	send_lcd_command(0x30);
 	HAL_Delay(1);
-	send_command(0x30);
+	send_lcd_command(0x30);
 
 	HAL_Delay(5);
-	send_command(0x20); // 4 bit mode
+	send_lcd_command(0x20); // 4 bit mode
 
 	HAL_Delay(5);
-	send_command(0x2C); // Function set
+	send_lcd_command(0x2C); // Function set
 
 	HAL_Delay(5);
-	send_command(0x08); // Display, cursor and blink, off
+	send_lcd_command(0x08); // Display, cursor and blink, off
 
 	HAL_Delay(5);
-	send_command(0x01); // Clear display
+	send_lcd_command(0x01); // Clear display
 
 	HAL_Delay(5);
-	send_command(0x0C); // Display on. Cursor and blink off
+	send_lcd_command(0x0C); // Display on. Cursor and blink off
 }
 
 /**
  *	@brief Send command to LCD, used for performing functions, clearing, setup, etc.
  *  @param cmd command byte to be sent
  */
-void send_command(uint8_t cmd)
+void send_lcd_command(uint8_t cmd)
 {
 	uint8_t upper_data, lower_data;
 
@@ -71,14 +71,14 @@ void send_command(uint8_t cmd)
 
 /**
  *	@brief Send data to LCD, used for ASIC characters
- *  @param cmd data to be sent
+ *  @param data data to be sent
  */
-void send_data(uint8_t data)
+void send_lcd_data(uint8_t data)
 {
 	uint8_t upper_data, lower_data;
 
-    upper_data = cmd & 0xf0; // 0xf0 -> 1111 0000, this gives first 4 bits
-	lower_data = (cmd << 4) & 0xf0; // Gives last four bits
+    upper_data = data & 0xf0; // 0xf0 -> 1111 0000, this gives first 4 bits
+	lower_data = (data << 4) & 0xf0; // Gives last four bits
 
 	uint8_t transfer_data[4]; // 4 bytes of data
 
@@ -96,11 +96,11 @@ void send_data(uint8_t data)
  *	@brief Send a string to be displayed on LCD
  *  @param *str pointer to string
  */
-void send_string(char *str)
+void send_lcd_string(char *str)
 {
     // Keep looping until the null-terminated string character reached indiating end of string
 	for (char *p = str; *p != '\0'; p++) {
-		send_data(*p); // Send each character
+		send_lcd_data(*p); // Send each character
 	}
 }
 
@@ -109,18 +109,18 @@ void send_string(char *str)
  *  @param row Row index (0 or 1)
  *  @param col Column index (0 to 15)
  */
-void set_cursor(uint8_t row, uint8_t col)
+void set_lcd_cursor(uint8_t row, uint8_t col)
 {
     // Base addresses for rows: 0x80 for row 0, 0xC0 for row 1, (0-1) rows and (0-15) columns
 	uint8_t base_address = (row == 0) ? 0x80 : 0xC0;
-    send_command(base_address + col);
+	send_lcd_command(base_address + col);
 }
 
 /**
  *	@brief Clears the LCD making it blank
  */
-void clear_display(void)
+void clear_lcd(void)
 {
-    send_command(0x01);
+	send_lcd_command(0x01);
 	HAL_Delay(2);
 }
