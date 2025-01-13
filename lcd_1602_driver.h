ea05281e44508938bed5afc0c479135fb47c68c9
Simar Singh Ubhi <73083235+simarubhi@users.noreply.github.com>
2025-01-13 17:24:10 -0500

    Names fixed header file

--- lcd_1602_driver.h
+++ lcd_1602_driver.h
@@ -15,38 +15,39 @@
 #define LCD_ADDRESS (0x27 << 1) // I2C address for LCD module (changes depending on specific model, check data sheet or store)
 
 /**
- * @brief Initializes the LCD in 4-bit mode.
+ *	@brief initalization sequence to lcd and assign I2C handle for driver
+ *  @param *hi2c the handle for the I2C lane chosen
  */
 void init_lcd(I2C_HandleTypeDef *hi2c);
 
 /**
- * @brief Sends a command to the LCD.
- * @param cmd Command to send
+ *	@brief Send command to LCD, used for performing functions, clearing, setup, etc.
+ *  @param cmd command byte to be sent
  */
-void send_command(uint8_t cmd);
+void send_lcd_command(uint8_t cmd);
 
 /**
- * @brief Sends a data byte (character) to the LCD.
- * @param data Data byte to send
+ *	@brief Send data to LCD, used for ASIC characters
+ *  @param data data to be sent
  */
-void send_data(uint8_t data);
+void send_lcd_data(uint8_t data);
 
 /**
- * @brief Sends a string to the LCD.
- * @param str Pointer to the null-terminated string (\0)
+ *	@brief Send a string to be displayed on LCD
+ *  @param *str pointer to string
  */
-void send_string(char *str);
+void send_lcd_string(char *str);
 
 /**
- * @brief Sets the cursor to a specific position on the LCD.
- * @param row Row index (0 or 1)
- * @param col Column index (0 to 15)
+ *	@brief Set position of cursor on LCD
+ *  @param row Row index (0 or 1)
+ *  @param col Column index (0 to 15)
  */
-void set_cursor(uint8_t row, uint8_t col);
+void set_lcd_cursor(uint8_t row, uint8_t col);
 
 /**
- * @brief Clears the LCD display.
+ *	@brief Clears the LCD making it blank
  */
-void clear_display(void);
+void clear_lcd(void);
 
 #endif /* INC_LCD_1602_DRIVER_H_ */
